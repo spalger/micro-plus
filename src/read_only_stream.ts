abc6770cd906fb06e36485b40a54ee1103fc20c1
spalger <email@spalger.com>
2019-05-29 13:50:25 -0700

    fix bad import

--- src/read_only_stream.ts
+++ src/read_only_stream.ts
@@ -1,7 +1,7 @@
 import { Readable } from 'stream'
 
 // @ts-ignore untyped simple module
-import readOnlyStream from 'read-only-stream'
+import * as readOnlyStream from 'read-only-stream'
 
 export function makeReadOnlyStream(stream: Readable): Readable {
   return readOnlyStream(stream)
